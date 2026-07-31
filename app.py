44284180f7a7e87c904d3e16eb5a813933ed7225
lgnrvz <lgnrvz@users.noreply.github.com>
2026-07-31 17:14:09 +0800

    Fix duplicate servers: disable debug reloader (use_reloader=False)

--- app.py
+++ app.py
@@ -1772,4 +1772,4 @@ def handle_react_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5050, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5050, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
